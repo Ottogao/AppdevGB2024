c4421161de1de3e8c2d8e378a875d387c1646488
Chao Gao <gc@vamk.fi>
2024-04-03 16:16:31 +0300

    new version

---
+++ PascalTriangle/pascal.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void pascal_triangle(int);
--- PascalTriangle/screen.h
+++ PascalTriangle/screen.h
@@ -2,5 +2,5 @@
 enum COLORS{BLACK=30,RED,GREEN,YELLOW,BLUE,MAGENTA,CYAN,WHITE};
 
 void gotoXY(int row, int col);
-
+void resetColors(void);
 void clearScreen(void);
